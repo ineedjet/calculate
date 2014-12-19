28ed95a3daeca0f6b3362769663e8f0fd32563f4
Yuriy Kirillov <yurkakirilov@gmail.com>
2014-12-19 22:25:55 +0100

    создал контроллер для рута

---
+++ features/calculator.feature
@@ -0,0 +1,5 @@
+Feature: Basic functionality of a calculator
+
+  Scenario: I can see button on the root page
+  	When I navigate to the root page
+  	Then I should see 'Calculate' button
---
+++ features/step_definitions/calculator_steps.rb
@@ -0,0 +1,7 @@
+When(/^I navigate to the root page$/) do
+  visit '/'
+end
+
+Then(/^I should see '(\S+)' button$/) do |name|
+  pending # express the regexp above with the code you wish you had
+end
