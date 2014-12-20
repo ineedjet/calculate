48a89316ade8117e278ce2ff80aecf5097e178be
Yuriy Kirillov <yurkakirilov@gmail.com>
2014-12-20 22:08:21 +0100

    Добавлено вычисление значения

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,9 +1,11 @@
 class RootController < ApplicationController
   def index
     @equation = '2+2'
+    @result = session[:last_result]
   end
 
-  def eval
+  def calc_eval
+    session[:last_result] = eval params[:equation]
     redirect_to root_path
   end
   
